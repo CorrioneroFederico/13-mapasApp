c59e4bad8ceb9770b2da18415638f5f27186fabd
CorrioneroFederico <corrionerofederico@gmail.com>
2023-02-12 20:57:00 -0300

    Dibujar la polyline

--- src/app/maps/services/map.service.ts
+++ src/app/maps/services/map.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
+import { AnySourceData, LngLatBounds, LngLatLike, Map, Marker, Popup } from 'mapbox-gl';
 import { Feature } from '../interfaces/places';
 import { DirectionsApiClient } from '../apis/directionsApiClient';
 import { DirectionsResponse, Route } from '../interfaces/directions';
@@ -90,5 +90,45 @@ export class MapService {
     this.map?.fitBounds(bounds, {
       padding: 200
     })
+
+    // Polyline
+    const sourceData: AnySourceData = {
+      type: 'geojson',
+      data: {
+        type: 'FeatureCollection',
+        features: [
+          {
+            type: 'Feature',
+            properties: {},
+            geometry: {
+              type: 'LineString',
+              coordinates: coords
+            }
+          }
+        ]
+      }
+    };
+
+    if (this.map.getLayer('RouteString')) {
+      this.map.removeLayer('RouteString');
+      this.map.removeSource('RouteString');
+    }
+
+    this.map.addSource('RouteString',sourceData);
+
+    this.map.addLayer({
+      id: 'RouteString',
+      type: 'line',
+      source: 'RouteString',
+      layout: {
+        "line-cap": 'round',
+        "line-join": 'round',
+      },
+      paint: {
+        "line-color": 'black',
+        "line-width": 3,
+      }
+    })
+
   }
 }
