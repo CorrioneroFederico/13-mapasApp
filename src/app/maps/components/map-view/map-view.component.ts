e7e47376f32f67f1ef0ef9d6f9d4dd7a5584b042
CorrioneroFederico <corrionerofederico@gmail.com>
2023-02-12 00:49:36 -0300

    Marcadores y Popups

--- src/app/maps/components/map-view/map-view.component.ts
+++ src/app/maps/components/map-view/map-view.component.ts
@@ -1,31 +1,37 @@
 import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import {Map} from 'mapbox-gl';
+import { Map, Popup, Marker } from 'mapbox-gl';
 import { PlacesService } from '../../services/places.service';
 
 @Component({
   selector: 'app-map-view',
   templateUrl: './map-view.component.html',
-  styleUrls: [ './map-view.component.css',
-  ]
+  styleUrls: ['./map-view.component.css'],
 })
 export class MapViewComponent implements AfterViewInit {
 
   @ViewChild('mapDiv')
   mapDivElement!: ElementRef;
 
-  constructor(
-    private _placesService:PlacesService
-  ) { }
+  constructor(private _placesService: PlacesService) {}
   ngAfterViewInit(): void {
-
-    if(!this._placesService.useLocation) throw Error('No hay _placesService.useLocation');
+    if (!this._placesService.useLocation)
+      throw Error('No hay _placesService.useLocation');
 
     const map = new Map({
       container: this.mapDivElement.nativeElement, // le mando el elemento html donde renderiza el mapa
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: this._placesService.useLocation, // starting position [lng, lat]
       zoom: 16, // starting zoom
-      });
-  }
+    });
 
+    const popup = new Popup().setHTML(`
+        <h6>Aquí estoy</h6>
+        <span>Estoy en este lugar del mundo</span>
+      `);
+
+    new Marker({ color: 'red' })
+      .setLngLat(this._placesService.useLocation)
+      .setPopup(popup)
+      .addTo(map);
+  }
 }
